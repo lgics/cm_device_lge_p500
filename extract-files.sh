44f7759127d31469ef4c8f3ba4c4c69fd3d90d4e
Android Meda <hephappy@gmail.com>
2012-06-18 14:39:35 +0300

    p500: inherit msm7x27-common blobs

--- extract-files.sh
+++ extract-files.sh
@@ -2,10 +2,15 @@
 
 VENDOR=lge
 DEVICE=p500
+COMMON=msm7x27-common
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+#rm -rf $BASE/*
 
+# Get common files
+(cd ../$COMMON && ./extract-files.sh)
+
+echo "Pulling device files..."
 for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
