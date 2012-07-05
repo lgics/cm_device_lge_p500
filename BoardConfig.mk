258ed4811346b34c1a7537b0d3b9e83200e2c802
Android Meda <hephappy@gmail.com>
2012-07-05 13:24:13 +0300

    frameworks/base/services/sensorservice/Android.mk:
    ifneq ($(filter p990 p999 p970, $(TARGET_BOOTLOADER_BOARD_NAME)),)
        LOCAL_CFLAGS += -DUSE_LGE_ALS_DUMMY
        ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),p970)
            LOCAL_CFLAGS += -DUSE_LGE_ALS_OMAP3
        endif
    endif

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,11 @@
 -include vendor/lge/p500/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
-
-# Nedeed for sensors
-COMMON_GLOBAL_CFLAGS += -DUSE_LGE_ALS_DUMMY
-
 TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
+BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 
 TARGET_BOOTLOADER_BOARD_NAME := p500
 TARGET_OTA_ASSERT_DEVICE := thunderg,p500
-
-BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 
 USE_CAMERA_STUB := false
 
