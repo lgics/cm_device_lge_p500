3ecfdd85a9998e16144a0dda8b4241ba6bd9f15a
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-10-06 22:04:14 -0400

    Bring in-line with new kernel configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 -include vendor/lge/p500/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_p500_p509_defconfig
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 
 TARGET_BOOTLOADER_BOARD_NAME := p500
